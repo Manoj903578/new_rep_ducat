064cf7bdc2caf3cc4c47ac88cdfb73304eb8c7a4
Manoj <manojyadavkumar290@gmail.com>
2025-04-26 12:16:13 +0530

    ec-2 instance

--- code1.py
+++ code1.py
@@ -1,7 +1,27 @@
-this 
-is 
-another 
-file 
-edit
+import boto3
 
-this is another file
+def launch_ec2_with_cloudformation(template_body, stack_name):
+  client = boto3.client('cloudformation')
+  response = client.create_stack(
+    StackName=stack_name,
+    TemplateBody=template_body,
+    Capabilities=['CAPABILITY_NAMED_IAM']
+  )
+  print(f"Stack creation initiated: {response['StackId']}")
+
+# Example usage
+template_body = """
+{
+  "AWSTemplateFormatVersion": "2010-09-09",
+  "Resources": {
+  "MyEC2Instance": {
+    "Type": "AWS::EC2::Instance",
+    "Properties": {
+    "InstanceType": "t2.micro",
+    "ImageId": "ami-0abcdef1234567890"
+    }
+  }
+  }
+}
+"""
+launch_ec2_with_cloudformation(template_body, "MyEC2Stack")
